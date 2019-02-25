7a6168848401899d8decbde5313e49cd3d9b9295
ubriand2018 <ubriand2018@51-UC46-01.ad.campus-eni.fr>
2019-02-25 16:46:21 +0100

    CreateManager

---
+++ src/fr/eni/enchere/bll/EnchereManager.java
@@ -0,0 +1,18 @@
+package fr.eni.enchere.bll;
+
+import fr.eni.enchere.bo.Utilisateur;
+import fr.eni.enchere.dal.UtilisateurDao;
+
+public class EnchereManager {
+	private UtilisateurDao utilisateurDao;
+	
+	public EnchereManager(UtilisateurDao utilisateurDao) {
+		this.utilisateurDao = utilisateurDao;
+	}
+	
+	public Utilisateur createUtilisateur() {
+		
+		
+		return null;
+	}
+}
