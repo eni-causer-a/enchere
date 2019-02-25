f8bfc3d1b20aa29c3f44696e4ac4ed1d4ab28ecb
tclavier2018 <tclavier2018@51-UC46-10.ad.campus-eni.fr>
2019-02-25 13:59:41 +0100

    update context html and add userdao

--- src/fr/eni/enchere/bo/Utilisateur.java
+++ src/fr/eni/enchere/bo/Utilisateur.java
@@ -123,5 +123,25 @@ public class Utilisateur {
 		this.articlesVendus = articlesVendus;
 		this.articlesAchetes = articlesAchetes;
 	}
+	public Utilisateur(int noUtilisateur, String pseudo, String nom, String prenom, String email, String telephone,
+			String rue, String codePostale, String ville, String motDePasse, int credit, boolean administareur) {
+		super();
+		this.noUtilisateur = noUtilisateur;
+		this.pseudo = pseudo;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.telephone = telephone;
+		this.rue = rue;
+		this.codePostale = codePostale;
+		this.ville = ville;
+		this.motDePasse = motDePasse;
+		this.credit = credit;
+		this.administareur = administareur;
+		
+	}
+	public Utilisateur() {
+		// TODO Auto-generated constructor stub
+	}
 	
 }
