92131110f9bc4e60721e06d74c455e3360ca3154
pdurrmey2018 <pdurrmey2018@51-UC46-22.ad.campus-eni.fr>
2019-02-28 13:40:47 +0100

    dqsSQsd

--- src/fr/eni/enchere/servlet/ServletConnexion.java
+++ src/fr/eni/enchere/servlet/ServletConnexion.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,7 +17,7 @@ import fr.eni.enchere.bo.Utilisateur;
 /**
  * Servlet implementation class ServletConnexion
  */
-@WebServlet("/ServletConnexion")
+@WebServlet("/Connexion")
 public class ServletConnexion extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -33,6 +34,19 @@ public class ServletConnexion extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		Cookie[] cookies = request.getCookies();
+		if(cookies != null) {
+		
+			for (Cookie cookie : cookies) {
+				String nom=cookie.getName();
+			
+				if (nom.equals("idCompte")) {
+					request.setAttribute("pseudo",cookie.getValue() );					
+				}
+			}
+		}
+		
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/Connexion.jsp");
 		rd.forward(request, response);
 		
@@ -45,16 +59,32 @@ public class ServletConnexion extends HttpServlet {
 		// TODO Auto-generated method stub
 		String id=request.getParameter("Identifiant");
 		String mdp=request.getParameter("MotDePasse");
+		String seSouvenirDeMoi=request.getParameter("SeSouvenirDeMoi");
 		
 		UtilisateurManager utilisateurManager=new UtilisateurManager();
 		Utilisateur utilisateur=utilisateurManager.getUtilisateur(id,mdp);
 		System.out.println(utilisateur);
 		if(utilisateur==null) {
-			response.sendRedirect(request.getContextPath()+"/ServletConnexion");
+			response.sendRedirect(request.getContextPath()+"/Connexion");
 			
 		}else {
 			HttpSession session= request.getSession();
 			session.setAttribute("Utilisateur", utilisateur);
+			
+			if(seSouvenirDeMoi!=null) {
+				Cookie cookie=new Cookie("idCompte",id);
+				cookie.setVersion(1);
+				cookie.setMaxAge(1000);
+				cookie.setPath("/Enchere/Connexion");
+				response.addCookie(cookie);
+			}else {
+				Cookie cookie=new Cookie("idCompte","");
+				cookie.setVersion(1);
+				cookie.setMaxAge(1000);
+				cookie.setPath("/Enchere/Connexion");
+				response.addCookie(cookie);
+			}
+			
 			response.sendRedirect(request.getContextPath()+"/Accueil");
 		}
 	}
